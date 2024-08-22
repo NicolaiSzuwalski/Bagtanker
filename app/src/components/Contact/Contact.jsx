6f33abc994a74e6051cf5c84ecade766d53d3b29
NicolaiSzuwalski <132432642+NicolaiSzuwalski@users.noreply.github.com>
2024-08-22 19:18:11 +0200

    ContactPageDone

--- app/src/components/Contact/Contact.jsx
+++ app/src/components/Contact/Contact.jsx
@@ -1,30 +1,27 @@
 import React from 'react'
 import styles from './Contact.module.scss'
+import map from '../../assets/Images/Map.png'
 
 export const Contact = () => {
   return (
     <div className={styles.ContactContainer}>
+
         <div className={styles.ContactForm}>
             <p>Udfyld og send formularen og vi vil hurtigst muligt besvare dine spørgsmål.</p>
-            
         <form>
-          <div className={styles.FormGroup}>
-            <input type="text" id="name" name="name" placeholder="Navn" required />
-          </div>
-          
-          <div className={styles.FormGroup}>
-            <input type="email" id="email" name="email" placeholder="Email" required />
-          </div>
-          
-          <div className={styles.FormGroup}>
-            <textarea id="message" name="message" rows="5" placeholder="Besked" required></textarea>
-          </div>
-          
-          <button type="submit" className={styles.SendButton}>Send</button>
+            <input type="text" id="name" name="name" placeholder="Indtast dit navn" required />
+         
+            <input type="email" id="email" name="email" placeholder="Indtast din email" required />
+         
+            <textarea id="message" name="message" rows="5" placeholder="Skriv en besked" required></textarea>
+    
+            <button type="submit" className={styles.SendButton}>Send</button>
         </form>
-
-
         </div>
+        <div className={styles.Map}>
+            <img src={map} alt="map" />
+        </div>
+
     </div>
   )
 }
